319b5fbed0659fa122bef4c45f29db7e5b8da86f
Arisen_Early-Coder <n09824730@gmail.com>
2026-03-14 14:15:03 +0530

    Add files via upload

---
+++ Quiz Excersise.py
@@ -0,0 +1,15 @@
+import random
+
+questions = [["What is 2+2? "], ["What comes after G? "], ["What is the first letter in the alphabets? "]]
+answers = [["A. 2, B. 3, C. 4, D. 22"], ["A. G, B. H, C. I, D. J"], ["A. a, B. b, C. z, D. y"]]
+guesses = []
+score = 0
+qna = []
+
+for q in questions:
+    qna.append(q)
+    for a in answers:
+        qna.append(a)
+        
+
+print(qna)
